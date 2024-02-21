b0a1db44e78bf4d2289470fe1feea42eb43c59f5
Khoi Nguyen <khoinguyen1942001@gmail.com>
2024-02-21 22:16:02 +0700

    Custom component TextField of MUI (update)

--- src/components/text-field/index.tsx
+++ src/components/text-field/index.tsx
@@ -4,6 +4,10 @@ import React from 'react';
 import { TextFieldProps, TextField } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
+/*
+ * ({ theme }) => { ... }: Hàm này nhận theme của Material-UI làm tham số và trả về object chứa các kiểu CSS
+ *
+ * */
 const TextFieldStyled = styled(TextField)<TextFieldProps>(({ theme }) => {
     console.log(theme);
 
@@ -31,6 +35,44 @@ const TextFieldStyled = styled(TextField)<TextFieldProps>(({ theme }) => {
             // Custom cho thẻ input
             '.MuiInputBase-input': {
                 padding: '8px 10px',
+            },
+
+            '&.Mui-error': {
+                borderColor: theme.palette.error.main,
+            },
+
+            '&.Mui-focused': {
+                boxShadow: theme.shadows[2],
+                '& .MuiInputBase-input:not(.MuiInputBase-readOnly):not([readonly])::placeholder': {
+                    transform: 'translateX(4px)',
+                },
+                '&.MuiInputBase-colorPrimary': {
+                    borderColor: theme.palette.primary.main,
+                },
+                '&.MuiInputBase-colorSecondary': {
+                    borderColor: theme.palette.secondary.main,
+                },
+                '&.MuiInputBase-colorInfo': {
+                    borderColor: theme.palette.info.main,
+                },
+                '&.MuiInputBase-colorSuccess': {
+                    borderColor: theme.palette.success.main,
+                },
+                '&.MuiInputBase-colorWarning': {
+                    borderColor: theme.palette.warning.main,
+                },
+                '&.MuiInputBase-colorError': {
+                    borderColor: theme.palette.error.main,
+                },
+                '&.Mui-error': {
+                    borderColor: theme.palette.error.main,
+                },
+            },
+            '&.Mui-disabled': {
+                backgroundColor: `${theme.palette.action.selected} !important`,
+            },
+            '& .MuiInputAdornment-root': {
+                marginTop: `0 !important`,
             },
         },
     };
